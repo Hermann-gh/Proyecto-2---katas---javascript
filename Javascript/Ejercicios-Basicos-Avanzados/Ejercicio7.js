fda695a50086c2a9fc618654540558bb61dc33fd
GermanVillar <german.villar.ramos@edu.thepower.education>
2026-05-29 11:33:14 +0200

    Añadir ejercicios 7 y 8 de Basico-Avanzado

---
+++ Javascript/Ejercicios-Basicos-Avanzados/Ejercicio7.js
@@ -0,0 +1,11 @@
+/*
+Completa esta función para que, al recibir dos números por argumento,
+te devuelva por consola el más alto de los dos.
+*/
+
+function greaterNumber(numberOne , numberTwo) {
+
+    return console.log(numberOne > numberTwo ? numberOne : numberTwo);
+}
+
+greaterNumber (17,23);
